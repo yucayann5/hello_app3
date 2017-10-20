29db99997a034f240924a6c43514f308096d447d
Masahiro Yamada <masahiro.yamada@dena.com>
2017-10-20 11:50:36 +0900

    trust周りのリファクタとか

--- app/controllers/trusts_controller.rb
+++ app/controllers/trusts_controller.rb
@@ -1,12 +1,47 @@
 class TrustsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @user_trusts = UserTrust.where(user_id: @user.id).to_a
+    @user_trusts = UserTrust.where(user_id: @user.id).order(updated_at: :desc).to_a
   end
 
   def new
     @user = User.find(params[:user_id])
-    messages = %W(
+    @user_trust = UserTrust.new(user_id: @user.id, from_user_id: current_user.id)
+    @message = get_message
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @user_trust = UserTrust.new(new_attributes)
+    @user_trust.user_id = @user.id
+    @user_trust.from_user_id = current_user.id
+
+    if @user_trust.invalid?
+      @message = get_message
+      return render :new
+    end
+
+    begin
+      UserTrust.transaction do
+        @user_trust.save!
+      end
+    rescue => e
+      logger.error(e.message)
+      return redirect_to user_trusts_path(@user)
+    end
+
+    flash[:notice] = "TRUSTを投稿しました！"
+    redirect_to user_trusts_path(@user)
+  end
+
+  private
+
+  def new_attributes
+    params.require(:user_trust).permit(:content)
+  end
+
+  def get_message
+    %W(
       #{@user.name}さんの強みは？
       #{@user.name}さんの性格は？
       #{@user.name}さんとの関係は？
@@ -15,25 +50,6 @@ class TrustsController < ApplicationController
       #{@user.name}さんは何をしているひと？
       #{@user.name}さんのスキルは？
       #{@user.name}さんをあなたが採用するなら、どんなポジションにしますか？
-    )
-    @message = messages.sample
-    @user_trust = UserTrust.new(user_id: @user.id)
-  end
-
-  def create
-    user = User.find(params[:user_id])
-    @user_trust = UserTrust.new(new_attributes)
-    @user_trust.user_id = user.id
-    @user_trust.from_user_id = current_user.id
-    @user_trust.save!
-
-    flash[:notice] = "TRUSTを投稿しました！"
-    redirect_to user_trusts_path(user)
-  end
-
-  private
-
-  def new_attributes
-    params.require(:user_trust).permit(:content)
+    ).sample
   end
 end
--- app/models/user_trust.rb
+++ app/models/user_trust.rb
@@ -1,3 +1,5 @@
 class UserTrust < ApplicationRecord
   has_one :from_user, primary_key: :from_user_id, foreign_key: :id, class_name: User
+
+  validates :content, presence: true, length: { maximum: 65535 }
 end
